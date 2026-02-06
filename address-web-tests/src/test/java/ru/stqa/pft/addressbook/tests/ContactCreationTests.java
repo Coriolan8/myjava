17b2786dcb6b28aec8705a4ea3954dd86f9e682b
Yulia <vereyulya@gmail.com>
2026-02-06 16:11:54 +0300

    Update ContactCreationTests.java

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,15 +4,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactCreationTests extends TestBase{
+public class ContactCreationTests extends TestBase {
   FirefoxDriver wd;
 
-    @Test
+  @Test
   public void testContactCreation() {
+    app.getNavigationHelper().goHomePage();
     app.getContactHelper().addNewContact();
     app.getContactHelper().fillContactForm(new ContactData("Yulia", "Ve", "9(888)777-66-55", "prelest@gm.com"));
     app.getContactHelper().submitContactCreation();
-    app.getNavigationHelper().returnHomePage();
+    app.getNavigationHelper().goHomePage();
   }
 
 }
